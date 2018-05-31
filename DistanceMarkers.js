207a30cab30acddf51bd8cc357896ab9321c8902
fullhdpixel <tsmudde@hotmail.com>
2018-05-31 20:26:27 +0200

    Initial commit

---
+++ DistanceMarkers.js
@@ -0,0 +1,31 @@
+import L from 'leaflet'
+import {Polyline} from 'react-leaflet'
+
+import './lib/leaflet-distance-marker.js'
+import './lib/leaflet-distance-marker.css'
+
+// Converts the leaflet-distance-marker to React Component
+export default class DistanceMarkers extends Polyline {
+  createLeafletElement(props) {
+    const {positions, ...otherProps} = props
+    const {map} = this.context
+
+    this.polyline = new L.Polyline(positions, {
+     ...otherProps
+    }).addTo(map)
+    return this.polyline
+  }
+
+  updateLeafletElement(fromProps, toProps) {
+    const {map} = this.context
+
+    this.polyline.removeFrom(map) // Resets
+    this.createLeafletElement(toProps) // To redraw Distance Markers
+  }
+}
+
+import PropTypes from 'prop-types'
+
+DistanceMarkers.propTypes = {
+  positions: PropTypes.array
+}
